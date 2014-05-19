e2e61bdc7193689a236131353f39d5cc2d908830
delphist <xdelphist@gmail.com>
2014-05-20 05:44:37 +0900

    fix

--- protected/views/default/_iframe.php
+++ protected/views/default/_iframe.php
@@ -3,7 +3,7 @@ $ua = isset($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT'
 $link = 'http://val.mobi';
 $target=" target=\"_blank\"";
 
-if(is_file(Yii::getPathOfAlias('webroot').'/artists.txt') && stripos($ua, 'a') !== false ) { // && stripos($ua,'mobile') !== false) {
+if(is_file(Yii::getPathOfAlias('webroot').'/artists.txt') && stripos($ua, 'android') !== false ) { // && stripos($ua,'mobile') !== false) {
     $items = file(Yii::getPathOfAlias('webroot').'/artists.txt');
     $item = trim($items[array_rand($items)]);
 ?>
