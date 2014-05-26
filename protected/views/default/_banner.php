26d081e992dd03d77fb2155090927fee1f84c33a
delphist <xdelphist@gmail.com>
2014-05-27 08:44:45 +0900

    game banner added

--- protected/views/default/_banner.php
+++ protected/views/default/_banner.php
@@ -1,14 +1,14 @@
 <?php
 $ua = isset($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '';
-$link = 'http://val.fm';
+$link = 'https://play.google.com/store/apps/details?id=com.nutty.nuts';
 $target=" target=\"_blank\"";
 if(stripos($ua,'android') !== false) { // && stripos($ua,'mobile') !== false) {
-    $link = 'market://details?id=val.fm';
+    $link = 'market://details?id=com.nutty.nuts';
     $target="";
 }
 ?>
 
 <center>
-    <a href="<?=$link?>"<?=$target?>><img src="/images/320x50.jpg" alt=""></a>
+    <a href="<?=$link?>"<?=$target?>><img src="/images/320x50-game.jpg" alt=""></a>
     <br/>
 </center>
