9585d524b0ad8072cb7a94f31848ea231cf37091
delphist <xdelphist@gmail.com>
2014-05-28 00:03:58 +0900

    UTM source added to banners

--- protected/views/default/_banner.php
+++ protected/views/default/_banner.php
@@ -1,9 +1,9 @@
 <?php
 $ua = isset($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '';
-$link = 'https://play.google.com/store/apps/details?id=com.nutty.nuts';
+$link = 'https://play.google.com/store/apps/details?id=com.nutty.nuts&referrer=utm_source%3Dmp3ler';
 $target=" target=\"_blank\"";
 if(stripos($ua,'android') !== false) { // && stripos($ua,'mobile') !== false) {
-    $link = 'market://details?id=com.nutty.nuts';
+    $link = 'market://details?id=com.nutty.nuts&referrer=utm_source%3Dmp3ler';
     $target="";
 }
 ?>
