a3f06010f412031ae12ef9362d8b007ccf0ed65c
delphist <xdelphist@gmail.com>
2014-06-02 22:31:32 +0900

    banner bugfixes

--- protected/views/default/_banner.php
+++ protected/views/default/_banner.php
@@ -1,14 +1,30 @@
 <?php
+$banners = array(
+    array(
+        'link' => 'https://play.google.com/store/apps/details?id=com.nutty.nuts&referrer=utm_source%3Dmp3ler',
+        'market' => 'market://details?id=com.nutty.nuts&referrer=utm_source%3Dmp3ler',
+        'image' => '/images/320x50-game.jpg',
+    ),
+    array(
+        'link' => 'https://play.google.com/store/apps/details?id=val.fm',
+        'market' => 'market://details?id=val.fm',
+        'image' => '/images/320x50.jpg',
+    ),
+);
+
+$random = array_rand($banners);
+$banner = $banners[$random];
+
 $ua = isset($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '';
-$link = 'https://play.google.com/store/apps/details?id=com.nutty.nuts&referrer=utm_source%3Dmp3ler';
+$link = $banner['link'];
 $target=" target=\"_blank\"";
 if(stripos($ua,'android') !== false) { // && stripos($ua,'mobile') !== false) {
-    $link = 'market://details?id=com.nutty.nuts&referrer=utm_source%3Dmp3ler';
+    $link = $banner['market'];
     $target="";
 }
 ?>
 
 <center>
-    <a href="<?=$link?>"<?=$target?>><img src="/images/320x50-game.jpg" alt=""></a>
+    <a href="<?=$link?>"<?=$target?>><img src="<?=$banner['image']?>" alt=""></a>
     <br/>
 </center>
