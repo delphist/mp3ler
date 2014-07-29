6260d9c0bb62c5d7899a3d3541c39894826d2f3b
delphist <xdelphist@gmail.com>
2014-07-29 21:04:16 +0900

    mp3fon & gettune fixes

--- protected/views/default/_flashes.php
+++
@@ -1,6 +0,0 @@
-<?php if(Yii::app()->user->hasFlash('success')) { ?>
-    <div class="alert alert-success alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <?=Yii::app()->user->getFlash('success')?>
-    </div>
-<?php } ?>
