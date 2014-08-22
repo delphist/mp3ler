201d896298bff1bb112b9a0a92237cd57782f282
delphist <xdelphist@gmail.com>
2014-08-23 01:33:29 +0900

    fixes

--- themes/mp3fon/views/TagBar/tagBar.php
+++ themes/mp3fon/views/TagBar/tagBar.php
@@ -1,7 +1,21 @@
 <div class="panel panel-default tag-cloud">
     <div class="panel-body">
+        <div class="row">
+            <div class="col-md-3">
+            <?php
+            $count = count($queries);
+            ?>
         <?php foreach($queries as $query) { ?>
-            <a href="<?=$this->getController()->createUrl('query/view', array('text' => $query->text)) ?>" title="<?=$query->title?>"><?=$query->title?></a>
+            <?php
+            $i++;
+            if($i % ceil($count / 4) == 0)
+            {
+                echo '</div><div class="col-md-3">';
+            }
+            ?>
+            <a href="<?=$this->getController()->createUrl('query/view', array('text' => $query->text)) ?>" title="<?=$query->title?>"><?=$query->title?></a><br />
         <?php } ?>
+            </div>
+        </div>
     </div>
 </div>
