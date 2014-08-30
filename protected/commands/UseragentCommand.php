99e95d5c01ee19b435ea80cc1fbcafa561175d09
delphist <xdelphist@gmail.com>
2014-08-30 18:41:35 +0900

    fixes

---
+++ protected/commands/UseragentCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+class UseragentCommand extends CConsoleCommand
+{
+    /**
+     * Экспортирует список юзерагентов в .txt файл
+     */
+    public function actionExport()
+    {
+        $file = fopen('useragent.txt', 'w+');
+
+        $command = Yii::app()->db->createCommand('SELECT * FROM useragent');
+        $reader = $command->query();
+
+        foreach($reader as $row)
+        {
+            fwrite($file, $row['title'].' --- '.$row['headers']);
+        }
+
+        fclose($file);
+    }
+}
